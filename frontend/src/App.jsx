83445f8bcc02aa7927980458c6d7b6ed479ada5e
ChutimunTorntranon <ChutimunTorntranon@gmail.com>
2024-04-17 01:57:21 +0700

    CreateRegisLoginForm&AuthContext

---
+++ frontend/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <>dsa</>;
+}
+
+export default App;
---
+++ frontend/src/context/authContext.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useEffect, useState, createContext, Children } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = () => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const login = async (inputs) => {
+    const res = await axios.post(
+      "http://localhost:8800/api/auth/login",
+      inputs,
+      {
+        withCredentials: true,
+      }
+    );
+    setCurrentUser(res.data);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login }}>
+      {Children}
+    </AuthContext.Provider>
+  );
+};
